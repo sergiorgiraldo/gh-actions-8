c1db46ffdf8ef3834a91e1ad1d1230299fa7137e
Sergio Giraldo <sergio@giraldo.com.br>
2022-11-18 10:07:58 +0100

    run custom js action:: by sergio giraldo @ 20221118T1007CET, gpg signed

---
+++ .github/actions/deploy-s3-javascript/main.js
@@ -0,0 +1,13 @@
+// Run at root
+// npm init -y
+// npm install @actions/core @actions/github @actions/exec
+
+const core = require("@actions/core");
+const github = require("@actions/github");
+const exec = require("@actions/exec");
+
+function run(){
+    core.notice("Hello from my custom action");
+}
+
+run();
