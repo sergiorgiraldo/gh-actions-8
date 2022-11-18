b50f93e3f56d66aa6d54fd1b245c1c686b1a774f
Sergio Giraldo <sergio@giraldo.com.br>
2022-11-18 14:17:21 +0100

    deploy to S3:: by sergio giraldo @ 20221118T1417CET, gpg signed

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -8,6 +8,13 @@ const exec = require("@actions/exec");
 
 function run(){
     core.notice("Hello from my custom action");
+
+    const bucket = core.getInput("bucket", {required: true});
+    const bucketRegion = core.getInput("bucket-region", {required: true});    
+    const distFolder = core.getInput("dist-folder", {required: true});
+    
+    const s3Uri = `s3://${bucket}`;
+    exec.exec(`aws s3 sync ${distFolder} ${bucket} --region ${bucketRegion}`);
 }
 
 run();
